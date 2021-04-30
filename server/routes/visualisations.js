9089648181007b9ff525a07121b8bf5b44adcc9f
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-30 12:46:25 +0200

    vis. route logic fixes

--- server/routes/visualisations.js
+++ server/routes/visualisations.js
@@ -2,30 +2,48 @@ const { Router } = require("express");
 const User = require("../models/User");
 const Visualisation = require("../models/Visualisation");
 const { catchAsync } = require("../middleware/errors");
+const { UnprocessableEntity } = require("../errors");
 const router = Router();
 
 router.get(
   "/",
   catchAsync(async (req, res) => {
-    const data = await Visualisation.find({ owner: req.user.id }, "id name");
+    const data = await Visualisation.find(
+      { owner: req.user.id },
+      "id name type"
+    );
     res.status(200).json({ data });
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    console.log("PARAMS:", req.params.id);
+    const { type, data } = await Visualisation.findById(req.params.id);
+    res.status(200).json({ type, data });
   })
 );
 
 router.post(
   "/create",
   catchAsync(async (req, res) => {
-    const { data } = req.body;
-    const vis = await Visualisation.create({ data });
-    res.status(201).json({ id: vis.id });
+    const { name, type, data } = req.body;
+    const vis = await Visualisation.create({
+      name,
+      data,
+      type,
+      owner: req.user.id,
+    });
+    res.status(201).json({ _id: vis._id, name, type });
   })
 );
 
+// DELETE doesn't take a body
 router.delete(
-  "/delete",
+  "/delete/:id",
   catchAsync(async (req, res) => {
-    const { id } = req.body;
-    await Visualisation.deleteOne({ id });
+    await Visualisation.deleteOne({ _id: req.params.id });
     res.status(204).send("resource deleted successfully");
   })
 );
@@ -33,8 +51,13 @@ router.delete(
 router.put(
   "/update",
   catchAsync(async (req, res) => {
-    const { id, data } = req.body;
-    await Visualisation.updateOne({ id }, { data });
+    const { _id, data } = req.body;
+    const doc = await Visualisation.findById(_id);
+    if (!doc) {
+      throw new UnprocessableEntity();
+    }
+    doc.data = data;
+    await doc.save();
     res.status(204).send("resource updated successfully");
   })
 );

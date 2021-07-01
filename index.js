243e20befbe5380a33d78b8d8d7a1c9a044536bf
Kavtorev <dmitrijkavtorev@gmail.com>
2021-07-01 15:11:22 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const { initApp, establishDbConnection } = require("./app");
 const { PORT } = require("./config/app");
